49b66c517c4406e2128e3faff4d1941aa225ee63
rafaelpaguiar <rafaelpaguiar@gmail.com>
2025-08-07 09:12:36 -0300

    Added individual views

--- django/ll_project/learning_logs/urls.py
+++ django/ll_project/learning_logs/urls.py
@@ -6,4 +6,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     #Página inicial
     path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
