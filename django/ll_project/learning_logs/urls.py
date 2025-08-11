4da80e9248a5fe77e25a63038b9b870ef8a12780
rafaelpaguiar <rafaelpaguiar@gmail.com>
2025-08-11 14:35:26 -0300

    Started topics form page.

--- django/ll_project/learning_logs/urls.py
+++ django/ll_project/learning_logs/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     #Página inicial
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
-    path('topics/<int:topic_id>/', views.topic, name='topic')
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
--- django/ll_project/learning_logs/views.py
+++ django/ll_project/learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
-
+from .forms import TopicForm
+ 
 # Create your views here.
 def index(request):
     """A página inicial para o Registro de Aprendizagem"""
@@ -16,3 +17,14 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
